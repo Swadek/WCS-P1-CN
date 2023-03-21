96ea5752cf711b15bdfa3ea24b1745e9213a531d
CedricPalacio <cedric.palacio@gmail.com>
2023-03-21 21:47:33 +0100

    modification de 3 fichiers en faisant le git pull

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-function 
+const burgerIcon = document.querySelector(".burger-icon");
+const sidenav = document.querySelector(".sidenav");
+
+burgerIcon.addEventListener ("click", derouleBurger);
+
+function derouleBurger () {
+  sidenav.style.display = "block";
+  }   
+
+
+
