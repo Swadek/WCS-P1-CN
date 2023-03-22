44966e15dea1807490daa1f6f0b2bbac5c144bc1
Antoine JULIAN <antoine.julian@hotmail.com>
2023-03-22 22:49:23 +0100

    Correction toggle

--- togglePage2.js
+++ togglePage2.js
@@ -1,28 +1,28 @@
-const red = document.getElementById("red");
+const red = document.querySelector(`.toggleLi:nth-child(2)`);
 red.addEventListener(
   "click",
   () => {
       const container1 = document.querySelector('.containerContenu');
       const container2 = document.querySelector('.containerContenu2');
       console.log("Red");
-      // container1.classList.remove('blue');
+      container1.classList.remove('blue');
       container1.classList.add('red');
       
-      // container2.classList.remove('blue');
+      container2.classList.remove('blue');
       container2.classList.add('red');
       })
 
 
-const blue = document.getElementById("blue");
+const blue = document.querySelector(`.toggleLi:nth-child(1)`);
 blue.addEventListener(
     "click",
     () => {
         const container1 = document.querySelector('.containerContenu');
         const container2 = document.querySelector('.containerContenu2');
       
-        // container1.classList.remove('red');
+        container1.classList.remove('red');
         container1.classList.add('blue');
       
-        // container2.classList.remove('red');
+        container2.classList.remove('red');
         container2.classList.add('blue');
       })
