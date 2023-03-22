a78c32d723142c055b531814bf90f62a500adc39
CedricPalacio <cedric.palacio@gmail.com>
2023-03-22 09:53:48 +0100

    finalisation css V1 page1 et JS

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const burgerIcon = document.querySelector(".burger-icon");
 const sidenav = document.querySelector(".sidenav");
 
-burgerIcon.addEventListener ("click", derouleBurger);
+// burgerIcon.addEventListener ("click", derouleBurger);
+
+// function derouleBurger () {
+//   sidenav.style.display = "block";
+//   }   
+
+burgerIcon.addEventListener ("click",derouleBurger);
 
 function derouleBurger () {
-  sidenav.style.display = "block";
-  }   
-
+  sidenav.classList.toggle("deroule-menu");
+}
 
 
