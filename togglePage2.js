b26fae82d4d47e18a41868ba9e868181c39b2910
Antoine JULIAN <antoine.julian@hotmail.com>
2023-03-23 09:38:00 +0100

    Ajout du changement Page 2

--- togglePage2.js
+++ togglePage2.js
@@ -5,12 +5,12 @@ red.addEventListener(
       const container1 = document.querySelector('.containerContenu');
       const container2 = document.querySelector('.containerContenu2');
       console.log("Red");
-    //   container1.classList.remove('blue');
-    //   container1.classList.add('red');
-      container1.style.display = "none"
-      container2.style.display = "block"
-      //   container2.classList.remove('blue');
-      //   container2.classList.add('red');
+      container1.classList.remove('blue');
+      container1.classList.add('red');
+    //   container1.style.display = "none"
+    //   container2.style.display = "block"
+        container2.classList.remove('blue');
+        container2.classList.add('red');
       })
       
       
@@ -21,9 +21,9 @@ red.addEventListener(
               const container1 = document.querySelector('.containerContenu');
               const container2 = document.querySelector('.containerContenu2');
               
-              // container1.classList.remove('red');
-              // container1.classList.add('blue');
-              container2.style.display = "none"
-        // container2.classList.remove('red');
-        // container2.classList.add('blue');
+              container1.classList.remove('red');
+              container1.classList.add('blue');
+            //   container2.style.display = "none"
+        container2.classList.remove('red');
+        container2.classList.add('blue');
       })
